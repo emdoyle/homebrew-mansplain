21be6561848a1edd4741f8aa118cfd19be5710d6
Evan <evanmdoyle@gmail.com>
2019-09-01 17:37:38 -0700

    Update package

--- mansplain.rb
+++ mansplain.rb
@@ -3,7 +3,7 @@ class Mansplain < Formula
   desc "Basically it explains stuff to you"
   homepage ""
   url "https://github.com/emdoyle/homebrew-mansplain/raw/master/mansplain.tar.gz"
-  sha256 "13bd3ff930734d0ab5554791fb97c43de3774231beb68716b60c742e8bfaeb13"
+  sha256 "159cf597e61eba64534047d6119528e297d5ec0c767acebfd02434de1b790078"
   version "1"
 
   depends_on "python"
